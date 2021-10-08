af5efb0d2ed667bfa06378d56a34dcc10ecd5542
Clahoz20 <73487890+Clahoz20@users.noreply.github.com>
2021-10-08 17:33:52 +0200

    Update AddEmployee.java

--- AddEmployee.java
+++ AddEmployee.java
@@ -1,123 +1,54 @@
-/**
- *
- * @authors “posar els vostres noms”
- */
-
 package jaumebalmes.employeestofile;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-
+/**
+ * main per gestionar les creacions d'Employee i guardar/llegir info a fitxer
+ * @author Roger
+ */
 public class AddEmployee {
     
     public static void main (String[] args) throws Exception{
         
-        Employee newEmployee = new Employee(); //instanciem newEmployee amb la classe //Employee
+        Employee[] personal = new Employee[3]; //Creem una array de 3 posicions de personal
       
-        File fitxer = new File("treballadors.txt"); //Objecte File on instanciem fitxer i creem el //nostre arxiu .txt per guardar les dades dels treballadors
-        BufferedWriter llista = new BufferedWriter(new FileWriter(fitxer, true)); //Class PrintWriter per escriure les dades al //fitxer creat
-
-
-        //Instanciar Scanner
-        Scanner sc = new Scanner(System.in);
-
-        //Posar variables necessàries per indicar si s’ha acabat o no el posar dades del //treballador
-        boolean salida = false;
-        boolean error;
-
-        do{
-
-        //Demanar el nom de l’empleat
-            System.out.println("Introdueix el nom: ");
-            newEmployee.setName(sc.nextLine());
-            llista.write(newEmployee.getName()+"\n");
-
-        //Demanar el cognom de l’empleat
-            System.out.println("Introdueix els cognoms: ");
-            newEmployee.setSurname(sc.nextLine());
-            llista.write(newEmployee.getSurname()+"\n");
-
-        //Demanar edat de l’empleat
-            do{
-                error=false;
-                System.out.println("Introdueix l'edat: ");
-                try{
-                    newEmployee.setAge(Integer.parseInt(sc.nextLine()));
-                }catch(Exception ex){
-                    System.out.println("Error, procureu escriure un enter.\n");
-                    error=true;
-                }
-            }while(error);
-            llista.write(newEmployee.getAge()+"\n");
-
-        //Demanar alçada de l’empleat
-            do{
-                error=false;
-                System.out.println("Introdueix l'alçada: ");
-                try{
-                    newEmployee.setHeight(Float.parseFloat(sc.nextLine()));
-                }catch(Exception ex){
-                    System.out.println("Error, procureu escriure l'alçada amb el format xx.xx\n");
-                    error=true;
-                }
-            }while(error);
-            llista.write(""+newEmployee.getHeight()+"\n");
-
-        //Demanar lloc de treball
-            System.out.println("Introdueix el lloc de treball: ");
-            newEmployee.setJob(sc.nextLine());
-            llista.write(newEmployee.getJob()+"\n");
-
-        //Demanar el salari
-            do{
-                error=false;
-                System.out.println("Introdueix el salari: ");
-                try{
-                    newEmployee.setSalary(Double.parseDouble(sc.nextLine()));
-                }catch(Exception ex){
-                    System.out.println("Error, procureu escriure el salari amb el format xx.xx\n");
-                    error=true;
-                }
-            }while(error);            
-            llista.write(""+newEmployee.getSalary()+"\n");
-
-        //Demanar si esta o no casat
-            do{
-                error = false;
-                System.out.println("Introdueix si està casat/da (S/N): ");
-                String c = sc.nextLine();
-                if (c.equalsIgnoreCase("S"))
-                    newEmployee.setMarried(true);
-                if (c.equalsIgnoreCase("N"))
-                    newEmployee.setMarried(false);
-                if (!c.equalsIgnoreCase("S") && !c.equalsIgnoreCase("N")){
-                    System.out.println("Respongui només amb 'S' o 'N'");
-                    error = true;
-                }
-            }while(error);
-            llista.write(""+newEmployee.isMarried()+"\n");
-
-        //Preguntar si s’hi volen posar més empleats
-            do{
-                error = false;
-                System.out.println("Vols introduir un altre treballador? (S/N)");
-                String c = sc.nextLine();
-                if (c.equalsIgnoreCase("N"))
-                    salida = true;
-                if (!c.equalsIgnoreCase("S") && !c.equalsIgnoreCase("N")){
-                    System.out.println("Respongui només amb 'S' o 'N'");
-                    error = true;
-                }
-            }while(error);
+        //Afegim els 3 elements a l'array personal
+        personal[0] = new Employee ( "Ana", "Rebollo", 34, 178, "Ingenyiera Tècnica", 1700, "no");
+//Cmpletar els altres 2 empleats amb la info que vulgueu com fet amb l’anterior        
+    
+        
+        //Construim fluse de dades d'ObjectOutputStream dintre de try-catch
+                   
+            //Creem fluxe de dades de l'objecte pel que viatjarà la info
             
-         } while (!salida);
-        llista.close();
+            
+            //Escriure l'objecte
+           
+                        
+            //Tancar el fluxe
+           
+            
+            
+            //Recuperar l'objecte creat amb ObjectOutputStream
+            
+            
+  //Creem un array per emmagatzemar l'objecte. Hem de fer casting ja que el readOPbject no es    //d'objecte Employye i la info que llegim la volem d'Employee
 
-        //Tancar amb mètode close() les instàncies creades amb PrintWriter i Scanner
+           
+            
+            //Tancar el fluxe
+           
+
+            //Utilitxem un bucle (foreach) per llegir l'array personal_recuperat ja que és un array
+            
+                     
+       
+
 
     }
+        
+        
 }
